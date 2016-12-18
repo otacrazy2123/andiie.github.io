19f37afcd2f2ca9475fa9f53b1451b59432dfcfc
Andiie-PC\Andiie <andre.sousa.tec@gmail.com>
2016-12-17 22:54:45 -0200

    ...

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 
-var app = angular.module("app", []);
+var app = angular.module("app", []).controller('Hello', function($scope, $http){
+	$http.get('http://rest-service.guides.spring.io/greeting')
+		.then(function(response){
+			$scope.greeting = response.data;
+		});
+})
 
 
-	$(document).ready(function(){
+$(document).ready(function(){
 
-		setTimeout(function(){
-				$('body').addClass('loaded');
-		},3000);
+	setTimeout(function(){
+			$('body').addClass('loaded');
+	},3000);
 
-	});
+});
